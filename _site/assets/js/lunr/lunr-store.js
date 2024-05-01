c0720562202786a0aeda445775d72b2f0ed38d1a
kkokkomii <128340234+kkokkomii@users.noreply.github.com>
2024-05-01 23:28:39 +0900

    _site에 package.json 추가

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   post와 single의 차이는 무엇일까요     ","categories": [],
+        "tags": [],
+        "url": "/test2/",
+        "teaser": null
+      },{
+        "title": "Image",
+        "excerpt":"layout : single   title : “이미지 넣는 연습해요”   잘 들어갔나요~?   확인이 필요해요     ","categories": [],
+        "tags": [],
+        "url": "/image/",
+        "teaser": null
+      }]
