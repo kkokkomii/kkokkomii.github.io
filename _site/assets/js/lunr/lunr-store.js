b8dbd45612b2b65d4530a6160041a33666cc41fa
kkokkomii <128340234+kkokkomii@users.noreply.github.com>
2024-05-06 20:50:54 +0900

    글목차 추가

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -5,9 +5,9 @@ var store = [{
         "url": "/test2/",
         "teaser": null
       },{
-        "title": "Image",
-        "excerpt":"layout : single   title : “이미지 넣는 연습해요”   잘 들어갔나요~?   확인이 필요해요    class MenuClass:     def __init__(self):        self.menu = ['','Americano', 'Latte', 'Espresso', 'Mocha', '식혜', '수정과']        self.price = [0,1500, 2000, 1700, 2500, 2000, 1900]        self.bill = [0, 10000, 5000, 1000]        self.total = 0      ","categories": [],
-        "tags": [],
-        "url": "/image/",
+        "title": "이미지 넣는 연습해요",
+        "excerpt":"와우~   잘 들어갔나요~?   확인이 필요해요   우히히 신기하네요   class MenuClass:     def __init__(self):        self.menu = ['','Americano', 'Latte', 'Espresso', 'Mocha', '식혜', '수정과']        self.price = [0,1500, 2000, 1700, 2500, 2000, 1900]        self.bill = [0, 10000, 5000, 1000]        self.total = 0   젤 높은 수준의 목차에요     ","categories": ["coding"],
+        "tags": ["python","vscode","jekyll"],
+        "url": "/coding/image/",
         "teaser": null
       }]
